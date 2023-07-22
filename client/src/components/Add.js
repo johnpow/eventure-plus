1a49b2ea3b17244af58139116c0aa0ea725ca7b7
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-07-21 20:52:16 -0700

    Updated add event button, created dropdown category search to sidebar.

--- client/src/components/Add.js
+++ client/src/components/Add.js
@@ -6,27 +6,20 @@ import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import styled from "@emotion/styled";
 import Avatar from '@mui/material/Avatar';
-import { DateRange, EmojiEmotions, Image, PersonAdd, VideoCameraBack } from "@mui/icons-material";
 import { Button, ButtonGroup, TextField } from "@mui/material";
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import CloseIcon from '@mui/icons-material/Close';
 import { useMutation } from '@apollo/client';
-import { ADD_SIGNUP } from '../utils/mutations';
 import { useState } from 'react';
 import { ADD_EVENT } from '../utils/mutations';
 import { QUERY_EVENTS, QUERY_ME } from '../utils/queries';
-import dayjs from 'dayjs';
 import Auth from '../utils/auth';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import OutlinedInput from '@mui/material/OutlinedInput';
+import { ListItem } from "@mui/material";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -38,7 +31,6 @@ const MenuProps = {
     },
   },
 };
-
 
 const StyledModal = styled(Modal) ({
     display: "flex",
@@ -52,8 +44,6 @@ const UserBox = styled(Box)(({ theme }) => ({
     alignItems: "center",
     marginBottom: "20px",
 }))
-
-
 
 const Add = () => {
     const [open, setOpen] = useState(false);
@@ -71,18 +61,14 @@ const Add = () => {
         update(cache, { data: { addEvent } }) {
             try {
                 const { events } = cache.readQuery({ query: QUERY_EVENTS });
-          
                 const updatedAddEvent = { ...addEvent, signups: [] };
-          
                 cache.writeQuery({
                   query: QUERY_EVENTS,
                   data: { events: [updatedAddEvent, ...events] },
                 });
             } catch (e) {
                 console.error(e);
-            }
-        },
-    });
+      }}});
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
@@ -113,6 +99,17 @@ const Add = () => {
         setOpen(false);
     };
 
+    const resetFormState = () => {
+        setFormState({
+          eventTitle: '',
+          eventText: '',
+          eventAuthor: '',
+          eventLocation: '',
+          eventDate: '',
+          eventCategory: '',
+        });
+      };
+
     const handleChange = (event) => {
         const { name, value } = event.target;
 
@@ -128,14 +125,12 @@ const Add = () => {
             eventDate: newValue,
         });
     };
-
-      
       
     return (
         <>  
             <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Tooltip 
-            title="Add Post" 
+            title="Create New Event" 
             sx={{position:"fixed", bottom:20, left:{xs:"calc(50% - 25px)", md:30}}}
             onClick={() => setOpen(true)}
             >
@@ -145,7 +140,8 @@ const Add = () => {
             </Tooltip>
             <StyledModal
             open={open}
-            onClose={() => setOpen(false)}
+            onClose={() => {setOpen(false);
+                resetFormState();}}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
             >
@@ -155,7 +151,8 @@ const Add = () => {
             borderRadius={5}
             >
                 <Stack direction="row" justifyContent="end" color={"text.secondary"}>
-                    <CloseIcon onClick={() => setOpen(false)}/>
+                    <CloseIcon onClick={() => {setOpen(false);
+                    resetFormState();}}/>
                 </Stack>
                 <Typography variant="h6" color={"gray"} textAlign={"center"}>Create Event</Typography>
                 <UserBox>
@@ -180,23 +177,26 @@ const Add = () => {
                 onChange={handleChange}
                 />
 
-                <FormControl fullWidth sx={{ display: 'flex', alignItems: 'center' }}>
-                  <InputLabel
-                  >Category</InputLabel>
+                <FormControl fullWidth >
+                <ListItem disablePadding sx={{marginBottom: '10px', marginLeft: '2px'}}>Choose a Category</ListItem>
                   <Select
                     name='eventCategory'
                     value={formState.eventCategory}
-                    sx={{
-                        width: "100%",
-                        marginBottom: "10px"
-                    }}
+                    sx={{ width: "100%", marginBottom: "10px", color: 'gray' }}
                     variant= "outlined"
                     placeholder="Category"
                     required
                     onChange={handleChange}
-                    input={<OutlinedInput label="Category" />}
                     MenuProps={MenuProps}
-                  >             
+                    displayEmpty
+                    renderValue={(selected) => {
+                        if (selected.length === 0) {
+                          return <em>Category</em>;
+                        }
+                        return selected;
+                    }}
+                  >   
+                  <MenuItem disabled value=""><em>Category</em></MenuItem>          
                     <MenuItem value={"Arts and Crafts"}>Arts and Crafts</MenuItem>
                     <MenuItem value={"Education"}>Education</MenuItem>
                     <MenuItem value={"Entertainment"}>Entertainment</MenuItem>
@@ -209,16 +209,6 @@ const Add = () => {
                     <MenuItem value={"Trivia"}>Trivia</MenuItem>
                   </Select>
                 </FormControl>
-
-                {/* <TextField
-                name='eventCategory'
-                value={formState.eventCategory}
-                sx={{width:"100%", marginBottom:"10px"}}
-                variant="outlined"
-                placeholder="Category"       
-                required 
-                onChange={handleChange}
-                /> */}
                 <TextField
                 name='eventLocation'
                 value={formState.eventLocation}
@@ -228,9 +218,10 @@ const Add = () => {
                 required 
                 onChange={handleChange}
                 />
+                <ListItem disablePadding sx={{marginBottom: '10px', marginLeft: '2px'}}>Date and Time</ListItem>
                 <DateTimePicker fullWidth 
                     sx={{ marginBottom:"10px" }}
-                    label="Date and Time"
+                    // label="Date and Time"
                     name='eventDate'
                     value={value}
                     onChange={handleDateChange}
