b3799fa902fbf2bf6423fff43e08a1695c829e78
John Pow <johnowed@gmail.com>
2023-07-22 08:15:55 -0700

    mobile friendly

--- client/src/pages/Home4.js
+++ client/src/pages/Home4.js
@@ -1,33 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography, Grid } from '@mui/material';
 import EventureBG from '../images/cards/eventureBG.png';
 
 function Home() {
   return (
-    <>
-      <img src={EventureBG} alt="Eventure Background" />
-        <Box
-          position="fixed"
-          top="0"
-          left="0"
-          right="0"
-          bottom="0"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          zIndex={9999}
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{
+        minHeight: '100vh',
+        backgroundImage: `url(${EventureBG})`, // Set the background image here
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+    >
+      <Box
+        textAlign="center"
+        p={4}
+        borderRadius={8}
+        boxShadow={4}
+        bgcolor="white"
+        maxWidth="400px"
+        width="90%"
+      >
+        <Typography variant="h4" gutterBottom>
+          Welcome to Eventure!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Your adventure starts here.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          component={Link}
+          to="/login"
+          style={{ marginTop: '20px' }}
+          fullWidth
         >
-          <Box bgcolor="white" p={4} borderRadius={8} boxShadow={4} textAlign="center">
-            <h2>Welcome to Eventure</h2>
-            <p>Your next adventure begins here.</p>
-            <Button variant="contained" color="primary" component={Link} to="/login">
-              Login
-            </Button>
-          </Box>
-        </Box>
-     
-    </>
+          Get Started
+        </Button>
+      </Box>
+    </Grid>
   );
 }
 
